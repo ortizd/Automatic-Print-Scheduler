78a57ed55588ca1008b4104b81a6d7a175325c06
ortizd <dariortiz_1@hotmail.com>
2023-01-30 10:05:13 +0100

    Main.py almost complete

--- main.py
+++ main.py
@@ -13,15 +13,12 @@ from watchdog.events import PatternMatchingEventHandler
 from tkinter import *
 from tkinter import filedialog
 
-
-
 root= Tk()
 root.title("Automatic print")
 root.geometry('450x450')
 root.config(bg="skyblue")
 
 folder_path= StringVar()
-#folder_path= "Not folder selected"
 
 #Obs class
 if __name__ == "__main__":
@@ -30,18 +27,24 @@ if __name__ == "__main__":
     ignore_directories = False
     case_sensitive = True
     my_event_handler = PatternMatchingEventHandler(patterns, ignore_patterns, ignore_directories, case_sensitive)
-#    my_event_handler.on_created = on_created
-
-    #go_recursively = True
-    #my_observer = Observer()
-    #my_observer.schedule(my_event_handler, path= folder_path.get(), recursive=go_recursively)
 #Obs class
 
-def browse_button():
+def button_browse():
     global folder_path
     filename= filedialog.askdirectory()
     folder_path.set(filename)
-    #print(filename)
+    
+
+def button_run():
+    if (len(folder_path.get())>0):
+        print(folder_path.get())
+        my_observer.start()
+        try:
+            while True:
+                time.sleep(1)
+        except KeyboardInterrupt:
+            my_observer.stop()
+            my_observer.join()
 
 my_frame= Frame(root, width=200, height=200)
 my_frame.grid(row=0, column=0, padx=10, pady=5)
@@ -50,8 +53,12 @@ my_frame.config(bg="red")
 lbl1= Label(my_frame, textvariable=folder_path)
 lbl1.grid(row=1, column=0, padx=5, pady=5)
 
-button2= Button(my_frame, text="Browse", command=browse_button)
-button2.grid(row=2, column=0, padx=5, pady=5)
+button_browse= Button(my_frame, text="Browse", command=button_browse)
+button_browse.grid(row=2, column=0, padx=5, pady=5)
+
+
+button_run= Button(my_frame, text="Run", command=button_run)
+button_run.grid(row=4, column=2, padx=5, pady=5)
 
 
 #Obs class
@@ -63,10 +70,11 @@ def on_created(event):
 
 my_event_handler.on_created = on_created
 
-
 go_recursively = True
 my_observer = Observer()
-my_observer.schedule(my_event_handler, path= folder_path.get(), recursive=go_recursively)
+my_observer.schedule(my_event_handler, path= folder_path.get()+"/", recursive=go_recursively)
+
+
 
 
 #Obs class
