bdc3e0983cdd3f599d026532edf4f34cc3fa5e50
ortizd <dariortiz_1@hotmail.com>
2023-02-08 13:50:32 +0100

    Show files accepted to be printed

--- Handler.py
+++ Handler.py
@@ -1,13 +1,17 @@
 from watchdog.observers import Observer
 import watchdog.events
 import os
+from tkinter import messagebox
 class Handler(watchdog.events.PatternMatchingEventHandler):
     def __init__(self):
-        patterns = ["*.jpg", "*.jpeg", "*.png", "*.bmp", "*.pdf", ".docx"]
+        patterns = ["*.jpg", "*.jpeg", "*.png", "*.bmp", "*.pdf", "*.docx"]
         ignore_patterns = None
-        ignore_directories = False
+        ignore_directories = True
         case_sensitive = True
         watchdog.events.PatternMatchingEventHandler.__init__(self,patterns, ignore_patterns, ignore_directories, case_sensitive)
     
     def on_created(self, event):
-        os.startfile(event.src_path, "print")
+        try:
+            os.startfile(event.src_path, "print")
+        except:
+            messagebox.showinfo(message="There is no application associated with the file", title="No printer")
