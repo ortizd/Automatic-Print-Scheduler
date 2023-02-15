8498972b6f67321692f6d5ec4b6f15d4427422b7
ortizd <dariortiz_1@hotmail.com>
2023-02-15 14:44:49 +0100

    Changed to Setup.py

--- setup.py
+++ setup.py
@@ -4,4 +4,4 @@ setup(name="Automatic Print",
       version="1.0",
       description="monitors a designated folder and promptly sends any new file to the printer",
       executables=[Executable("MainGUI.pyw")],
-      options={"build_exe": {"include_files": ["config.json", "image.ico"]}})
+      options={"build_exe": {"include_files": ["config.json", "printer.ico"]}})
