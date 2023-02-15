5a150c0f6295fa89f25428cac32fd0422dc5d42b
ortizd <dariortiz_1@hotmail.com>
2023-02-15 13:53:30 +0100

    Changed to config.json

--- JSONReader.py
+++ JSONReader.py
@@ -3,10 +3,10 @@ class JsonReader:
         
     # Create read and write functios to use from the other files
     def read_json(self):
-        with open("permanent_file.json", "r") as file:
+        with open("config.json", "r") as file:
             data = json.load(file)
         return data
 
     def write_json(self, data):
-        with open("permanent_file.json", "w") as file:
+        with open("config.json", "w") as file:
             data= json.dump(data, file, indent=4)
